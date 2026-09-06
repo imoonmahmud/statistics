14ee2c918212d30d0d2c86f9963bdf2ea80003f1
imoonmahmud <imoonmahmud@gmail.com>
2026-09-06 13:00:47 +0600

    learned the box and whiskers plot

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Quartiles and interquartile range (IQR)
-data = pd.Series([2, 4, 6, 8, 10, 12, 14, 16, 50, 70])
+nums = [16, 18, 28, 13, 50, 31, 25, 22, 18, 23, 29, 38]
+data = pd.Series(nums)
+sorted_data = data.sort_values(ascending=True)
 
-q1 = data.quantile(0.25)
-q3 = data.quantile(0.75)
+q1 = sorted_data.quantile(0.25)
+q3 = sorted_data.quantile(0.75)
 iqr = q3 - q1
-
-print(f"Q1: {q1}, Q3: {q3}, IQR: {iqr}")
 
 lower = q1 - 1.5 * iqr
 upper = q3 + 1.5 * iqr
 
-outliers = data[(data < lower) | (data > upper)]
-print(outliers)
- 
+outliers = sorted_data[(sorted_data < lower) | (sorted_data > upper)]
+
+
+# Box and Whisker Plot
+plt.boxplot(sorted_data, vert=False)
+plt.title('Box Plot Example')
+plt.show()
